2fee7fe8204e056385948afd2b7344992ff83dbb
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-09 11:53:07 +0100

    wtf

--- tp_oriente_objet.py
+++ tp_oriente_objet.py
@@ -1,19 +1,22 @@
 class concessionnaire:
 
-    def __init__ (self, marque, nb_employes, ville, nb_modele):
+    def __init__(self, marque, nb_employes, ville, nb_modele):
         self.marque = marque
         self.nb_employes = nb_employes
         self.ville = ville
         self.nb_modele = nb_modele
-        self.voitures= []
+        self.voitures = []
+        self.moteurs = {}
 
-    def ajout_voiture(self,prix, roue, couleur, moteur):
-        self.voitures.append( Voiture(prix, roue, moteur, self.marque, couleur))
+    def ajout_voiture(self, prix, roue, couleur, moteur):
+        self.voitures.append(Voiture(prix, roue, moteur, self.marque, couleur))
 
-    def ajout_moteur (self, chevaux, carburant, catégorie):
-        
+    def ajout_moteur(self, chevaux, carburant, catégorie, nom):
+        self.moteurs[nom] = Moteur(chevaux, carburant, catégorie)
+
+
 class Voiture:
-    
+
     def __init__(self, prix, roue, moteur, marque, couleur):
         self.prix = prix
         self.marque = marque
@@ -21,11 +24,11 @@ class Voiture:
         self.roue = roue
         self.couleur = couleur
 
-    
+
 class Moteur:
-        
-    def __init__ (self, chevaux, carburant, catégorie):
-        self.chevaux= chevaux
+
+    def __init__(self, chevaux, carburant, catégorie):
+        self.chevaux = chevaux
         self.carburant = carburant
         self.type = catégorie
 
