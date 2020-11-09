d25689accd85c51002c20a41db615187d59c6ddf
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-09 11:14:18 +0100

    test class in class

--- tp_oriente_objet.py
+++ tp_oriente_objet.py
@@ -1,9 +1,14 @@
 class concessionnaire:
 
-    def __init__ (self, marque, nb_employes, ville):
+    def __init__ (self, marque, nb_employes, ville, nb_modele):
         self.marque = marque
-        self.nb_employes= nb_employes
-        self.ville= ville
-
+        self.nb_employes = nb_employes
+        self.ville = ville
+        self.nb_modele = nb_modele
+        self.voitures = voiture()
 
     class voiture:
+        
+        def __init__(self, prix, roue, moteur, self.marque):
+            self.prix = prix
+            self.marque = marque
