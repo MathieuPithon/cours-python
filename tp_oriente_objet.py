c6624cc788b9c911d86761ca9834f989f013baf3
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-09 10:51:55 +0100

    création fichier cours python tp

---
+++ tp_oriente_objet.py
@@ -0,0 +1,9 @@
+class concessionnaire:
+
+    def __init__ (self, marque, nb_employes, ville):
+        self.marque = marque
+        self.nb_employes= nb_employes
+        self.ville= ville
+
+
+    class voiture:
