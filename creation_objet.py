4c9cd8da5c1a2464016780a7409ce0518937f42f
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-09 12:01:31 +0100

    création d'un objet

--- creation_objet.py
+++ creation_objet.py
@@ -1,2 +1,7 @@
 from tp_oriente_objet import concessionnaire, Voiture, Moteur
 
+concession= concessionnaire("renault", 15, "Angers", 3)
+concession.ajout_moteur(150, "diesel", 2, "moteur à explosion numéro 1")
+concession.ajout_moteur(320, "sans plomb 95", 1, "gros moteur")
+concession.ajout_voiture(15000, "crantée", "bleu", moteurs["gros moteur"])
+print(concession.voitures[0].couleur)
