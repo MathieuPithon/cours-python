a61fab233c7055d114709e9e4dae95385b193551
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-09 12:50:32 +0100

    ajout de commentaires partie 2

--- tp_oriente_objet.py
+++ tp_oriente_objet.py
@@ -1,3 +1,4 @@
+# création de la classe concessionnaire
 class concessionnaire:
 
     def __init__(self, marque, nb_employes, ville, nb_modele):
@@ -8,13 +9,16 @@ class concessionnaire:
         self.voitures = []
         self.moteurs = {}
 
+    # fonction permettant d'ajouter les voitures en vente par la concession
     def ajout_voiture(self, prix, roue, couleur, moteur):
         self.voitures.append(Voiture(prix, roue, moteur, self.marque, couleur))
 
+    # fonction permettant d'enregistrer tout les types de moteurs présent dans les voitures de la concession
     def ajout_moteur(self, chevaux, carburant, catégorie, nom):
         self.moteurs[nom] = Moteur(chevaux, carburant, catégorie)
 
 
+# création de la classe voiture
 class Voiture:
 
     def __init__(self, prix, roue, moteur, marque, couleur):
@@ -24,7 +28,7 @@ class Voiture:
         self.roue = roue
         self.couleur = couleur
 
-
+# création de la classe moteur
 class Moteur:
 
     def __init__(self, chevaux, carburant, catégorie):
@@ -32,6 +36,6 @@ class Moteur:
         self.carburant = carburant
         self.type = catégorie
 
-
+# prévenir l'utilisateur si par erreur il exécute le mauvais fichier
 if __name__ == "__main__":
     print("le programme n'est pas fait pour ça")
